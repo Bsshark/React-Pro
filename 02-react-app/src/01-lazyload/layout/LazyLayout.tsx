f47dbb02286908c0b3a95af8e2feecbb4be0b354
Abraham <abrahamlega@gmail.com>
2023-11-29 12:10:40 +0000

    Arreglado aun mas el problema

--- 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
+++ 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
@@ -1,28 +1,50 @@
 import { LazyPage1, LazyPage2, LazyPage3 } from "../pages";
-import { Link, NavLink, Route, Router, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  Link,
+  NavLink,
+  Navigate,
+  Route,
+  Router,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import routes from "../../routes/routes";
+import { NavItemLi } from "../components/NavItemLi";
 
 export const LazyLayout = () => {
-    const url = useLocation();
-    const navigate = useNavigate();
+  const routesLazy = ["lazy1", "lazy2", "lazy3"];
   return (
     <div>
       <h1>LazyLayout Page</h1>
       <ul>
-        <li>
-          <Link to='/lazyload/lazy1'>Lazy 1</Link>
+        {routesLazy.map((routeLazy) => {
+          return (
+            <NavItemLi path={routes[0].to.concat(routeLazy)} name={routeLazy} />
+          );
+        })}
+
+        {/* <li>
+          <NavLink to={routes[0].to.concat('lazy1')} className={({ isActive }) => (isActive ? "nav-active" : "")}>Lazy 1</NavLink>
         </li>
         <li>
-          <Link to='/lazyload/lazy2'>Lazy 2</Link>
+          <NavLink to={routes[0].to.concat('lazy2')} className={({ isActive }) => (isActive ? "nav-active" : "")}>Lazy 2</NavLink>
         </li>
         <li>
-          <Link to='/lazyload/lazy3'>Lazy 3</Link>
-        </li>
+          <NavLink to={routes[0].to.concat('lazy3')} className={({ isActive }) => (isActive ? "nav-active" : "")}>Lazy 3</NavLink>
+        </li> */}
       </ul>
 
       <Routes>
-        <Route path="lazy1" element={<LazyPage1/>} />
-        <Route path="lazy2" element={<LazyPage2/>} />
-        <Route path="lazy3" element={<LazyPage3/>} />
+        <Route path="lazy1" element={<LazyPage1 />} />
+        <Route path="lazy2" element={<LazyPage2 />} />
+        <Route path="lazy3" element={<LazyPage3 />} />
+
+        {/* <Route path="/*" element={<div>Not found</div>} /> */}
+        <Route
+          path="*"
+          element={<Navigate replace to={routes[0].to.concat("lazy1")} />}
+        />
       </Routes>
     </div>
   );
