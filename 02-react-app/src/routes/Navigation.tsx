67f19666d32a3d588491ba1c08320f863ed8ba5c
Abraham <abrahamlega@gmail.com>
2023-11-29 23:00:39 +0000

    69. Reseteado a pagina main inicial. Terminado patron compound

--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 
 import logo from "../logo.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 export const Navigation = () => {
   return (
@@ -14,34 +13,34 @@ export const Navigation = () => {
             <ul>
               <li>
                 <NavLink
-                  to="/lazy1"
+                  to="/home"
                   className={({ isActive }) => (isActive ? "nav-active" : "")}
                 >
-                  Lazy 1
+                  Home
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  to="/lazy2"
+                  to="/about"
                   className={({ isActive }) => (isActive ? "nav-active" : "")}
                 >
-                  Lazy 2
+                  About
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  to="/lazy3"
+                  to="/users"
                   className={({ isActive }) => (isActive ? "nav-active" : "")}
                 >
-                  Lazy 3
+                  Users
                 </NavLink>
               </li>
             </ul>
           </nav>
           <Routes>
-            <Route path="lazy1" element={<LazyPage1/>} />
-            <Route path="lazy2" element={<LazyPage2/>} />
-            <Route path="lazy3" element={<LazyPage3/>} />
+            <Route path="home" element={<h1>Home</h1>} />
+            <Route path="about" element={<h1>About</h1>} />
+            <Route path="users" element={<h1>Users</h1>} />
             <Route path="/*" element={<Navigate to="/lazy1" replace />} />
           </Routes>
         </div>
