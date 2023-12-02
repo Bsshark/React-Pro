b24dd5e7fe42693a8025714a7263d3a4681af7b0
Abraham <abrahamlega@gmail.com>
2023-12-02 18:50:23 +0000

    113. Preparado el build del componente en npm

--- 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,7 +4,6 @@ import {
 	ProductImage,
 	ProductTitle,
 } from "../components/";
-import "../styles/custom-styles.css";
 import { products } from "../data/products";
 
 const product = products[0];
@@ -16,26 +15,22 @@ export const ShoppingPage = () => {
 			<hr />
 			<ProductCard
 				product={product}
-				className="bg-dark text-white"
 				key={product.id}
 				initialValues={{
 					quantity: 4,
 					maxQuantity: 10,
 				}}
 			>
-				{({reset, increaseBy, isMaxCountReached, count}) => (
+				{({ reset, increaseBy, isMaxCountReached, count }) => (
 					<>
-						<ProductImage
-							className="custom-image"
-							style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-						/>
-						<ProductTitle className="text-bold" />
-						<ProductButtons className="custom-buttons" />
-						<button onClick={reset}>Reset</button>
+						<ProductImage />
+						<ProductTitle />
+						<ProductButtons />
+						{/* <button onClick={reset}>Reset</button>
 						<button onClick={() => increaseBy(-2)}>-2</button>
 
 						<button style={{visibility: isMaxCountReached?'hidden':'visible'}}>+2</button>
-						<span>{count}</span>
+						<span>{count}</span> */}
 					</>
 				)}
 			</ProductCard>
