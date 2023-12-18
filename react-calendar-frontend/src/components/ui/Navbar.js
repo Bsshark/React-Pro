8c1e4202fad81e275ceb377ab3a9679d05477fd0
Abraham <abrahamlega@gmail.com>
2023-12-18 23:17:28 +0000

    212. Finalizada configuración de workbox

--- react-calendar-frontend/src/components/ui/Navbar.js
+++ react-calendar-frontend/src/components/ui/Navbar.js
@@ -1,30 +1,39 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { startLogout } from '../../actions/auth';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { startLogout } from "../../actions/auth";
+
+import { Offline, Online } from "react-detect-offline";
+import { PatchCheckFill, PatchExclamationFill } from "react-bootstrap-icons";
 
 export const Navbar = () => {
+	const dispatch = useDispatch();
+	const { name } = useSelector((state) => state.auth);
 
-    const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
+	const handleLogout = () => {
+		dispatch(startLogout());
+	};
 
-    const handleLogout = () => {
-        dispatch( startLogout() );
-    }
-
-    return (
-        <div className="navbar navbar-dark bg-dark mb-4">
-            <span className="navbar-brand">
-                { name }
-            </span>
-            
-            <button 
-                className="btn btn-outline-danger"
-                onClick={ handleLogout }
-            >
-                <i className="fas fa-sign-out-alt"></i>
-                <span> Salir</span>
-            </button>
-
-        </div>
-    )
-}
+	return (
+		<div className="navbar navbar-dark bg-dark mb-4">
+			<span className="navbar-brand">{name}</span>
+			<Online>
+				<span style={{ paddingRight: "2px" }}>
+					<PatchCheckFill color="green" />
+				</span>
+				<span className="text-success">Online</span>
+			</Online>
+			<Offline>
+				<span style={{ paddingRight: "2px" }}>
+					<PatchExclamationFill color="red" />
+				</span>
+				<span className="text-danger">
+					Offline - Peticiones serán guardadas
+				</span>
+			</Offline>
+			<button className="btn btn-outline-danger" onClick={handleLogout}>
+				<i className="fas fa-sign-out-alt"></i>
+				<span> Salir</span>
+			</button>
+		</div>
+	);
+};
--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -2,20 +2,57 @@ importScripts(
 	"https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
+workbox.loadModule("workbox-background-sync");
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+const cacheNetworkFirst = ["/api/auth/renew", "/api/events"];
+
+registerRoute(({ request, url }) => {
+	if (cacheNetworkFirst.includes(url.pathname)) return true;
+}, new NetworkFirst());
+/* 
+registerRoute(
+	({ request, url }) => url.pathname.startsWith("/api/auth/"),
+	new NetworkFirst()
+);*/
+
+const bgSyncPlugin = new BackgroundSyncPlugin("posts-offline", {
+	maxRetentionTime: 24 * 60,
+});
+
+const cacheFirst = [
+	"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css",
+];
+
+registerRoute(({ request, url }) => {
+	if (cacheFirst.includes(url.href)) return true;
+}, new CacheFirst());
+
+//Posteos offline
 
 registerRoute(
-	new RegExp(
-		"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-	),
-	new CacheFirst()
+	new RegExp("http://localhost:4000/api/events"),
+	new NetworkOnly({
+		plugins: [bgSyncPlugin],
+	}),
+	"POST"
 );
 registerRoute(
-	new RegExp(
-		"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
-	),
-	new CacheFirst()
+	new RegExp("http://localhost:4000/api/events"),
+	new NetworkOnly({
+		plugins: [bgSyncPlugin],
+	}),
+	"PUT"
+);
+registerRoute(
+	new RegExp("http://localhost:4000/api/events"),
+	new NetworkOnly({
+		plugins: [bgSyncPlugin],
+	}),
+	"DELETE"
 );
